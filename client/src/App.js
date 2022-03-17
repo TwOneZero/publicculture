e7510bf677ce0a3fdfe5a27f9768a84f60a64301
Chae <pce2416@gmail.com>
2022-03-17 11:21:20 +0900

    게시물 디테일 페이지 디자인

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import PostPage from './components/views/PostPage/PostPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path='/' element={<LandingPage />} />
         <Route exact path='/login' element={<LoginPage />} />
         <Route exact path='/register' element={<RegisterPage />} />
+        <Route exact path='/post' element={<PostPage />} />
       </Routes>
     </div>
   );
