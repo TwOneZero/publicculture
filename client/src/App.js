372358fcfa02c5d5e9b6250b2d465345e9019ac5
Mihee <vip0020@naver.com>
2022-07-20 15:03:12 +0900

    마이페이지

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import PostPage from './components/views/PostPage/PostPage';
-import ShowEvent from './components/views/ShowEvent/ShowEvent';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import PostPage from "./components/views/PostPage/PostPage";
+import ShowEvent from "./components/views/ShowEvent/ShowEvent";
+import Mypage from "./components/views/Mypage/Mypage";
+import MypageEdit from "./components/views/Mypage/MypageEdit";
+import MypageInfo from "./components/views/Mypage/MypageInfo";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path='/' element={<LandingPage />} />
-        <Route exact path='/login' element={<LoginPage />} />
-        <Route exact path='/register' element={<RegisterPage />} />
-        <Route exact path='/post/:postId' element={<PostPage />} />
-        <Route exact path='/showevent/' element={<ShowEvent />} />
-        <Route exact path='/showevent/:name' element={<ShowEvent />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/post/:postId" element={<PostPage />} />
+        <Route exact path="/showevent/" element={<ShowEvent />} />
+        <Route exact path="/showevent/:name" element={<ShowEvent />} />
+        <Route exact path="/mypage" element={<Mypage />} />
       </Routes>
     </div>
   );
