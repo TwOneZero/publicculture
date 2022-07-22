7bd72747dd58ec923474912609a3da4e47fbd036
twoonezero <dnjsdudtm98@naver.com>
2022-07-22 15:28:04 +0900

    랜덤 포스트 추출, event 라우터 삭제

--- server/app.js
+++ server/app.js
@@ -19,7 +19,6 @@ app.use(cors());
 
 //router imports
 const userRouter = require('./routes/user');
-// const eventRouter = require('./routes/event');
 const postRouter = require('./routes/post');
 const commentRouter = require('./routes/comment');
 
@@ -33,7 +32,6 @@ mongoose
 
 //라우팅 url
 app.use('/api', userRouter);
-// app.use('/api', eventRouter);
 app.use('/api', postRouter);
 app.use('/api', commentRouter);
 
