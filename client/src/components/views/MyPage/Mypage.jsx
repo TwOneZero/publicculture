8a19f011e70c7d7f76625ad2b596f95f9300f0a2
KimJunSik <pkwaw@naver.com>
2022-08-17 14:06:19 +0900

    my page 수정

--- client/src/components/views/MyPage/Mypage.jsx
+++ client/src/components/views/MyPage/Mypage.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux"; //내 액션을 한 번에 모아서 처리. 이 기능이
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import MypageEdit from "./MypageEdit";
-import MypageInfo from "./MypageInfo";
-import MyLikedPost from "./MyLikedPost";
-import Auth from "../../../hoc/auth";
-import { auth } from "../../../_actions/user_action";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'; //내 액션을 한 번에 모아서 처리. 이 기능이
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import MypageEdit from './MypageEdit';
+import MypageInfo from './MypageInfo';
+import Auth from '../../../hoc/auth';
+import { auth } from '../../../_actions/user_action';
+import axios from 'axios';
 const MypageBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -18,7 +17,7 @@ const MypageBox = styled.div`
   width: 100%;
   height: 100%;
   font-size: 17px;
-  font-family: "Noto Sans KR", sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
 `;
 
 const UserBox = styled.div`
@@ -28,7 +27,7 @@ const UserBox = styled.div`
   align-items: center;
   height: 300px;
   color: white;
-  //padding-left: 100px;
+  padding-left: 100px;
 `;
 
 const UserBoxContainer = styled.div`
@@ -43,8 +42,7 @@ const MypageTitle = styled.div`
 
 const UserInfoBox = styled.div`
   display: flex;
-  //width: 100%;
-  //width로 가운데 정렬이 되는지 왼쪽 정렬되는지 가운데 정렬되는지 바뀜
+  width: 100%;
 `;
 
 const UserBtnBox = styled.div`
@@ -170,7 +168,7 @@ const InfoBox = styled.div`
 `;
 
 function Mypage() {
-  const [mode, setMode] = useState("myProfile");
+  const [mode, setMode] = useState('myProfile');
   const dispatch = useDispatch();
   const [resData, setResData] = useState(null);
   const [userData, setUserData] = useState(null);
@@ -178,31 +176,34 @@ function Mypage() {
   useEffect(() => {
     dispatch(auth())
       .then((res) => {
-        setResData(res.payload);
-        //console.log(res.payload);
-      })
+      setResData(res.payload);
+    })
       .catch((error) => {
         console.log(error);
-      });
-  }, [dispatch]);
+      })
+  },[dispatch]);
 
   useEffect(() => {
     if (resData) {
       setUserData(resData);
-      //console.log(resData);
     }
-  }, [resData]);
+  },[resData])
+
+  //관심행사 테스트
+  const testGetFav = () => {
+    axios.get('/api/likedPost').then((res) => console.log(res.data));
+  };
 
   const onMenuButtonClick = (e) => {
-    if (e.target.id === "myProfile") {
-      setMode("myProfile");
+    if (e.target.id === 'myProfile') {
+      setMode('myProfile');
       console.log(e.target.id);
-    } else if (e.target.id === "editProfile") {
-      setMode("editProfile");
-    } else if (e.target.id === "myLikedPost") {
-      setMode("myLikedPost");
-    } else if (e.target.id === "mycomment") {
-      setMode("mycomment");
+    } else if (e.target.id === 'editProfile') {
+      setMode('editProfile');
+    } else if (e.target.id === 'likedpostings') {
+      setMode('likedpostings');
+    } else if (e.target.id === 'mycomment') {
+      setMode('mycomment');
     }
   };
 
@@ -213,35 +214,35 @@ function Mypage() {
         <UserInfoBox>
           <UserBtnBox>
             <UserIcon>
-              <i className="fa-solid fa-user"></i>
+              <i className='fa-solid fa-user'></i>
             </UserIcon>
-            <MyinfoBtn onClick={onMenuButtonClick} id="myProfile">
+            <MyinfoBtn onClick={onMenuButtonClick} id='myProfile'>
               My Info
             </MyinfoBtn>
           </UserBtnBox>
           <UserNamePreferBox>
-            {userData ? <UserName>{userData.name}</UserName> : ""}
+            {userData ? <UserName>{userData.name}</UserName> : ''}
 
-            <PreferenceBox>선호 장르 : 콘서트</PreferenceBox>
+            <PreferenceBox>선호 장르 : {userData ? userData.genre : ""}</PreferenceBox>
           </UserNamePreferBox>
           <UserInfoMenuBtns>
-            <UserInfoEditBox onClick={onMenuButtonClick} id="editProfile">
+            <UserInfoEditBox onClick={onMenuButtonClick} id='editProfile'>
               <UserInfoEditIcon>
-                <i className="fa-solid fa-gear"></i>
+                <i className='fa-solid fa-gear'></i>
               </UserInfoEditIcon>
               <UserInfoEditTitle>내 정보 수정</UserInfoEditTitle>
             </UserInfoEditBox>
 
-            <LikedBox onClick={onMenuButtonClick} id="myLikedPost">
+            <LikedBox onClick={onMenuButtonClick} id='likedpostings'>
               <LikeIcon>
-                <i className="fa-solid fa-heart"></i>
+                <i className='fa-solid fa-heart'></i>
               </LikeIcon>
               <LikeTitle>나의 관심 행사</LikeTitle>
             </LikedBox>
 
-            <CommentBox onClick={onMenuButtonClick} id="mycomment">
+            <CommentBox onClick={onMenuButtonClick} id='mycomment'>
               <CommentIcon>
-                <i className="fa-solid fa-comment"></i>
+                <i className='fa-solid fa-comment'></i>
               </CommentIcon>
               <CommentTitle> 내가 쓴 댓글 </CommentTitle>
             </CommentBox>
@@ -250,9 +251,8 @@ function Mypage() {
       </UserBox>
       <InfoBox>
         <div>
-          {mode === "myProfile" && <MypageInfo></MypageInfo>}
-          {mode === "editProfile" && <MypageEdit></MypageEdit>}
-          {mode === "myLikedPost" && <MyLikedPost></MyLikedPost>}
+          {mode === 'myProfile' && <MypageInfo></MypageInfo>}
+          {mode === 'editProfile' && <MypageEdit></MypageEdit>}
         </div>
       </InfoBox>
     </MypageBox>
