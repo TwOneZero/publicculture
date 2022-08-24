5cfa17705e3a5d461ddedbdaa3fddc5fcee223c4
twonezero <dnjsdudtm98@naver.com>
2022-08-24 13:29:10 +0900

    comment수정

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,23 +1,32 @@
 const { Comment } = require('../schemas/Comment');
+const { Post } = require('../schemas/Post');
 const mongoose = require('mongoose');
 
 //댓글 달기
 exports.addComment = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { postId } = req.params;
     if (!req.user) {
       return res.status(404).json({ message: '유저 정보가 필요합니다' });
     }
-    if (!mongoose.Types.ObjectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
       return res.status(404).send('post 가져오기 에러');
     }
     //comment 인스턴스
     const comment = new Comment({
       userId: req.user._id,
       //post ObjectId
-      post: id,
+      post: postId,
       //client form 에서 comment field 로 넘겨주기
       body: req.body.comment,
+    });
+
+    const post = await Post.findOneAndUpdate(
+      postId,
+      { comment: comment },
+      { new: true }
+    ).then(() => {
+      console.log('post저장 성공');
     });
 
     //comment db에 저장
--- server/routes/comment.js
+++ server/routes/comment.js
@@ -5,6 +5,6 @@ const { auth } = require('../middlewares/auth');
 const router = express.Router();
 
 //comment 달기
-router.post('/comment', auth, addComment);
+router.post('/comment/:postId', auth, addComment);
 
 module.exports = router;
