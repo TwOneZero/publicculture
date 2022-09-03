cf1937c7d8b2a6314b2f34f109e45c186e7bd153
KimJunSik <pkwaw@naver.com>
2022-09-03 22:40:09 +0900

    캘린더 구조 수정중

--- client/src/components/views/Calendar/CalenderElements.js
+++ client/src/components/views/Calendar/CalenderElements.js
@@ -1,4 +1,6 @@
 import styled, { css } from 'styled-components';
+import {BsFillArrowLeftCircleFill as LeftArrow} from 'react-icons/bs';
+import {BsFillArrowRightCircleFill as RightArrow} from 'react-icons/bs';
 
 export const Container = styled.div`
     width: 1500px;
@@ -28,31 +30,18 @@ export const Text = styled.h3`
 
 export const Frame = styled.div`
     width: 1500px;
-    height: 1001px;
-    border: 1px solid lightgrey;
+    // border: 1px solid lightgrey;
     margin: 0 auto 0;
-    box-shadow: 2px 2px 2px #eee;
+    // box-shadow: 2px 2px 2px #eee;
 `
 
-export const Header = styled.div`
-    font-size: 18px;
-    font-weight: bold;
-    padding: 10px 10px 5px 10px;
-    display: flex;
-    justify-content: space-between;
-    background-color: #fff;
-`
-
-export const Button = styled.div`
-    cursor: pointer;
-`
 
 export const Body = styled.div`
     width: 100%;
-    padding: 0 1rem;
+    // padding: 0 1rem;
     display: flex;
     flex-direction: column;
-    border: 1px solid #000;
+    // border: 1px solid #000;
     box-sizing: border-box;
 `
 
@@ -75,24 +64,60 @@ export const Day_week = styled.div`
     justify-content: center;
     cursor: pointer;
     font-size: 16px;
-    border: 1px solid #000;
+    border: 1px solid #ccc;
 `
 
-export const TopContainer = styled.a`
-    display: block;
-    position: relative;
 
+
+
+
+export const Header = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    padding: 10px 10px 5px 10px;
+    // display: flex;
+    // justify-content: space-between;
+    background-color: #fff;
+`
+
+export const Month_container = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const Month = styled.div`
+    margin: 0 60px;
+    font-size: 60px;
+`
+
+export const Month_sub = styled.span`
+    font-size: 30px;
+`
+
+export const LeftArrows = styled(LeftArrow)`
+    display: flex;
+    cursor: pointer;
+    margin: 25px 0 0 0;
+    font-size: 30px;
+    
+`
+
+export const RightArrows = styled(RightArrow)`
+    display: flex;
+    cursor: pointer;
+    font-size: 30px;
+    margin: 25px 0 0 0;
 `
 
 export const SubContainer = styled.div`
     width: 14%;
     height: 177px;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
     cursor: pointer;
     font-size: 16px;
-    border: 1px solid #000;
+    border: 1px solid #ccc;
     ${props =>
         props.isToday &&
         css`
@@ -101,56 +126,38 @@ export const SubContainer = styled.div`
 
     ${props =>
         props.isSelected &&
-        css`
-        background-color: #eee;
-        `};
+        css`background-color: #eee;`
+    };
+`
+
+export const TopContainer = styled.div`
+    margin: 1rem;
 `
 
 export const Day = styled.span`
-    position: absolute;
-    top: -73px;
-    
-    
     font-size: 30px;
     line-height: 1;
 `
 
-
 export const AllEvent = styled.span`
-position: absolute;
-width: 100px;
-left: 85px;
-top: -70px;
-font-size: 24px;
-text-align: center;
-line-height: 1;
--webkit-transform: translate(-50%, 0);
-transform: translate(-50%, 0);
+    width: 100px;
+    font-size: 24px;
+    text-align: center;
+    line-height: 1;
+    -webkit-transform: translate(-50%, 0);
+    transform: translate(-50%, 0);
 `
 
 export const BottomContainer = styled.ul`
-    display: block;
+    padding-left: 1.8rem;
     list-style: none;
-    margin: 0;
-    padding: 0;
-    word-break: keep-all;
-    text-decoration: none;
-    font-weight: normal;
-    text-indent: inital;
-    word-wrap: break-word;
-    line-height: 1.3;
-    font-size: 14px;
+
 `
 
 export const Events = styled.li`
-float: left;
-width: 50%;
-color: #666;
-font-size: 14px;
-padding: 0;
-
-
-    &:nth-child(2n+1){
-        clear: left;
-    }
+    float: left;
+    width: 50%;
+    color: #666;
+    font-size: 15px;
+    line-height: 1.5rem;
 `
