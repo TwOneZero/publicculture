f85ab35a257c36f23a26b4490c6a69f4f729d03a
KimJunSik <pkwaw@naver.com>
2022-09-18 23:10:01 +0900

    posts state 해결중

--- client/src/_reducers/post_reducer.js
+++ client/src/_reducers/post_reducer.js
@@ -20,7 +20,7 @@ export default function postState(state = initialState, action) {
     case SEARCH_POST:
       return { posts: action.payload.posts };
     case DETAIL_POST:
-      return { post: action.payload.post };
+      return { ...state, post: action.payload.post };
     case DETIAL_RANDOM_POST:
       return { posts: action.paylod.posts };
     case LIKE_POST:
