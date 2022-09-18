5acc11868ad0b8d7f18422d4e8119aa7c7247cb4
KimJunSik <pkwaw@naver.com>
2022-09-18 18:37:42 +0900

    postpage detail 수정중

--- client/src/components/views/PostPage/PostElements.js
+++ client/src/components/views/PostPage/PostElements.js
@@ -5,19 +5,103 @@ export const PostContainer = styled.div`
   align-items: center;
   font-family: 'Noto Sans KR';
   flex-direction: column;
-  margin: 60px;
+  margin-top: 50px;
 `
 
 export const PostContent = styled.div`
   display: flex;
   flex-direction: column;
+  width: 1300px;
 `
+
+export const WrapContainer = styled.div`
+  width: 1250px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+export const EventLContainer = styled.div`
+  max-width: 900px;
+  margin-left: 20px;
+`
+
+export const EventRContainer = styled.div`
+  width: 400px;
+  height: 600px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+
+export const RecommendContainer = styled.div``
+
+export const RecommendH1 = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`
+
+export const RecommendContent = styled.div`
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    align-items: flex-start;
+    grid-gap: 36px;
+    width: 300px;
+`
+
+export const RecommendList = styled.div`
+  background: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 10px;
+    max-height: 340px;
+    transition: all 0.2s ease-in-out;
+`
+export const RcImage = styled.img`
+  width: 180px;
+  height: 150px;
+  object-fit: cover;
+`
+
+export const RcH2 = styled.h2`
+  font-size: 1.5rem;
+  width: 150px;
+  margin: 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`
+
+export const RcP = styled.p`
+  font-size: 1rem;
+  width: 150px;
+  text-align: center;
+  margin: 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`
+
+export const ContainerH1 = styled.h1`
+  color: #000;
+  font-size: 26px;
+  font-weight: bold;
+`
+
+export const Line = styled.div`
+    border-bottom: 1px solid #000;
+`
+
+
+
 
 //행사 정보
 export const Event_title = styled.div`
   font-family: 'Noto Sans KR', sans-serif;
-  font-size: 45px;
-  font-weight: 700;
+  font-size: 26px;
+  font-weight: normal;
+  margin-top: 20px;
   margin-bottom : 10px;
   //background-color: yellow;
 `;
@@ -36,7 +120,7 @@ export const Photo_container = styled.img`
   width: 300px;
   height: 400px;
   margin: 25px 20px 20px 0;
-  object-fit: fill;
+  object-fit: cover;
 `;
 
 export const Event_info = styled.div`
@@ -51,6 +135,21 @@ export const Event_info_content = styled.div`
   font-weight: 400;
   //background-color: yellow;
 `;
+
+export const Event_Button = styled.a`
+  display: flex;
+  width: 150px;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #4e8cff;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: #ebf4ff;
+  cursor: pointer;
+  text-decoration: none;
+  color: #000;
+`
 
 //좋아요
 export const Like_container = styled.div`
