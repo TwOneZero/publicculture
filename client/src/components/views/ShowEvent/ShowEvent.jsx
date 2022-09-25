366c2c380dd50a8e48574ffdc4b6e511f6142c71
KimJunSik <pkwaw@naver.com>
2022-09-25 22:26:24 +0900

    sort 완성 및 컴포넌트 생성

--- client/src/components/views/ShowEvent/ShowEvent.jsx
+++ client/src/components/views/ShowEvent/ShowEvent.jsx
@@ -6,20 +6,24 @@ import { sortedPost, searchPost } from '../../../_actions/post_action';
 import Pagination from '../Pagination/Pagination';
 import Posts from './Posts';
 
-import { PostingContainer, PostingPiginationBox } from './ShowEventElements';
+import { 
+  PostingContainer, 
+  PostingPiginationBox,
+  SortContainer,
+  SortSelect,
+  SortOption,
+} from './ShowEventElements';
 import { useEffect } from 'react';
+
 
 const ShowEvent = () => {
   //navigate 로 넘긴 데이터를 useLocation 으로 받는다.
-  const location = useLocation();
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [Sorted, setSorted] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 15;
-  // const [loading, setLoading] = useState(false);
-  // const infos = location.state.infos;
+
 
   const indexOfLast = currentPage * postsPerPage;
   const indexOfFirst = indexOfLast - postsPerPage;
@@ -33,37 +37,43 @@ const ShowEvent = () => {
     dispatch(searchPost(name)).then((res) => {
       console.log(res.payload);
       setSorted(false);
+      
     });
   }, [dispatch, name]);
-
+  
+  
   const postState = useSelector((state) => state.post);
 
-  const onSortClicked = (e) => {
+  const handleChange = (e) => {
     e.preventDefault();
     let name = e.target.name;
     let mode = e.target.value;
+    if(mode === '기본'){
+      setSorted(false);
+      navigate(`/showevent/${name}`)
+    }
     dispatch(sortedPost(name, mode)).then((res) => {
       if (res.payload.success) {
-        navigate(`${mode}`);
+        navigate(`/showevent/${name}/${mode}`);
         setSorted(true);
       }
     });
-  };
+  }
 
   return (
+    <>
+    
     <PostingPiginationBox>
-      <button name='뮤지컬' value='likes' onClick={onSortClicked}>
-        test
-      </button>
-      <button
-        onClick={() => {
-          setSorted(false);
-        }}
-      >
-        취소
-      </button>
+      
+      
       <PostingContainer>
-        {/* <Posts posts={currentPosts(infos.posts)}></Posts> */}
+      <SortContainer>
+        <SortSelect name={name} onChange={handleChange}>
+          <SortOption value='기본'>기본</SortOption>
+          <SortOption value='likes'>좋아요순</SortOption>
+          <SortOption value='comments'>댓글순</SortOption>
+        </SortSelect>
+      </SortContainer>
         {Sorted === false && postState ? (
           <Posts posts={currentPosts(postState.posts)}></Posts>
         ) : (
@@ -76,6 +86,7 @@ const ShowEvent = () => {
         paginate={setCurrentPage}
       ></Pagination>
     </PostingPiginationBox>
+    </>
   );
 };
 
