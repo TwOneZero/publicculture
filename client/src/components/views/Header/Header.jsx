33d026c88809640aea6b526a52a6d3e98c510550
Mihee <vip0020@naver.com>
2022-09-26 00:41:44 +0900

    검색바 수정

--- client/src/components/views/Header/Header.jsx
+++ client/src/components/views/Header/Header.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { searchPost } from '../../../_actions/post_action';
-import { logout } from '../../../_actions/user_action';
-import Auth from '../../../hoc/auth';
-import Sidebar from './Sidebar';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { searchPost } from "../../../_actions/post_action";
+import { logout } from "../../../_actions/user_action";
+import Auth from "../../../hoc/auth";
+import Sidebar from "./Sidebar";
 import logo from "../../../assets/image/logo.png";
 import { FaBars } from "react-icons/fa";
 import {
@@ -22,57 +22,55 @@ import {
   SidebarButtonToggle,
   SidebarGenreBox,
   ToggleButton,
-
   GenreShadowBox,
-} from './HeaderElements';
+} from "./HeaderElements";
 
 function Header() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [isSidebarOpened, setIsSidebarOpened] = useState(false);
   const codenames = [
-    '국악',
-    '뮤지컬',
-    '전시',
-    '연극',
-    '콘서트',
-    '클래식',
-    '무용',
-    '축제',
-    '문화교양',
+    "국악",
+    "뮤지컬",
+    "전시",
+    "연극",
+    "콘서트",
+    "클래식",
+    "무용",
+    "축제",
+    "문화교양",
   ];
   const onSidebarToggleButtonClicked = () => {
     setIsSidebarOpened(!isSidebarOpened);
   };
 
   const onLogoClicked = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const onLoginbtnClicked = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   const onRegiterClicked = () => {
-    navigate('/register');
+    navigate("/register");
   };
 
   const onMypageClicked = () => {
-    navigate('/mypage');
+    navigate("/mypage");
   };
   const logOut = () => {
     dispatch(logout()).then((res) => {
       if (res.payload.isAuth === false) {
-        alert('이미 로그아웃 상태입니다.!');
+        alert("이미 로그아웃 상태입니다.!");
       } else {
-        alert('로그아웃 성공');
-        navigate('/');
+        alert("로그아웃 성공");
+        navigate("/");
       }
     });
   };
-
 
   const onGenreClicked = (e) => {
     e.preventDefault();
@@ -85,6 +83,7 @@ function Header() {
   };
 
   const onChangeSearch = (e) => {
+    e.preventDefault();
     setSearch(e.target.value);
   };
 
@@ -94,9 +93,10 @@ function Header() {
       if (res.payload.success) {
         navigate(`showevent/${search}`, { state: { infos: res.payload } });
       } else {
-        return new Response({ error: 'error!' });
+        return new Response({ error: "error!" });
       }
     });
+    setSearch("");
   };
 
   return (
@@ -108,21 +108,21 @@ function Header() {
         />
       )}
       <HeaderContainer>
-        <HeaderLogo type='button' onClick={onLogoClicked}>
+        <HeaderLogo type="button" onClick={onLogoClicked}>
           <LogoImg src={logo} />
         </HeaderLogo>
 
         <SearchBarArea onSubmit={onSearchClicked}>
-          <label htmlFor='search'></label>
+          <label htmlFor="search"></label>
           <SearchBar
             onChange={onChangeSearch}
-            name='search'
+            name="search"
             value={search}
-            type='text'
-            placeholder='장르 / 제목 / 장소'
+            type="text"
+            placeholder="장르 / 제목 / 장소"
           ></SearchBar>
-          <Button type='submit'>
-            <i className='fas fa-search'></i>
+          <Button type="submit">
+            <i className="fas fa-search"></i>
           </Button>
         </SearchBarArea>
         <MenuContainer>
@@ -143,7 +143,6 @@ function Header() {
       </HeaderContainer>
       <GenreShadowBox>
         <GenreContainer>
-
           <SidebarGenreBox>
             <SidebarButtonToggle>
               <ToggleButton onClick={onSidebarToggleButtonClicked}>
@@ -152,14 +151,20 @@ function Header() {
             </SidebarButtonToggle>
 
             <GenreBar>
-              {
-                codenames.map((item, index) => {
-                  return <GenreBtn key={index} itemType='button' onClick={onGenreClicked} name={item}>{item}</GenreBtn>
-                })
-              }
+              {codenames.map((item, index) => {
+                return (
+                  <GenreBtn
+                    key={index}
+                    itemType="button"
+                    onClick={onGenreClicked}
+                    name={item}
+                  >
+                    {item}
+                  </GenreBtn>
+                );
+              })}
             </GenreBar>
           </SidebarGenreBox>
-
         </GenreContainer>
       </GenreShadowBox>
     </>
